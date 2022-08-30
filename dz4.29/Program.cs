b09b762cff42003621388fb7e4d5b47b787f5570
«Svetlana» <desko.nn@gmail.com>
2022-08-30 16:32:05 +0300

    Выполнили задачу 29

---
+++ dz4.29/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void WriteArray(int[] array)
+    {
+        for(int i = 0;i<array.Length;i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    static void Main(string[] args)
+{
+    int[] array = new int[0];
+    while (true)
+    {
+        Console.WriteLine("Введите значение массива ");
+        string meaning = Console.ReadLine();
+        if(meaning != "end")
+        {
+            int number = Convert.ToInt32(meaning);
+            int[] newArray = array;
+            array = new int[array.Length + 1];
+            for(int i = 0; i < newArray.Length; i++)
+            {
+                array[i] = newArray[i];
+            }
+            array[array.Length-1] = number;
+        
+        }else{
+            break;
+        }
+    }WriteArray(array);
+}
+}
+
