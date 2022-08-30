aedbc4c7143ef1e5f51f5ce83f8affc80fca3580
«Svetlana» <desko.nn@gmail.com>
2022-08-30 15:40:58 +0300

    Выполнили задачу 27

---
+++ dz4.27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum =0;
+
+while(number > 0)
+{ 
+    int num = number % 10;
+    number = number / 10;
+    sum = sum + num;
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
