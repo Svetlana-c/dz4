a73634d890b00e9adf9738ee739c9f0038946da1
«Svetlana» <desko.nn@gmail.com>
2022-08-30 14:19:29 +0300

    Выполнили задачу 25

---
+++ dz4.25/Program.cs
@@ -0,0 +1,14 @@
+﻿int number(string num)
+{
+    Console.Write($"Введите число {num}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = number("A");
+int B = number("B");
+
+int pow = 1;
+for(int i = 0; i < B; i++)
+{
+    pow = pow * A;
+}
+Console.WriteLine($"{A} ^ {B} = {pow}");
